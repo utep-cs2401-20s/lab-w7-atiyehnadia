080965f7b2e63c8a2b20362fc40d6cd870f15079
Nadia <atiyeh.nadiaa@gmail.com>
2020-03-02 14:46:51 -0700

    started the SortOfSort method

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,2 +1,20 @@
 public class SortOfSort {
+    public void sortOfSort(int [] array){
+        int biggestNumber = 0;
+        biggestNumber = findMax(array);
+        int [] tempArray = new int[array.length];
+
+
+    }
+
+    public int findMax(int [] array){
+        int maximum;
+        for(int i = 0; i < array.length - 1; i++){
+            maximum = array[0];
+            if (array[i] > maximum){
+                maximum = array[i];
+            }
+        }
+        return maximum;
+    }
 }
